425d837a081e57fcfc4ae5b382806bfbff094c2c
mathison42 <mathison42@gmail.com>
2018-01-07 15:51:51 -0500

    Additional Sounds

--- App.js
+++ App.js
@@ -14,6 +14,15 @@ export default class App extends React.Component {
         <View style={styles.buttons}>
             <FlatList
                 data={[
+                    {key: 'Average',                            sound: require('./native/sounds/average.mp3')},
+                    {key: 'Dalton Smith Wannabe',               sound: require('./native/sounds/dalton-smith-wannabe.mp3')},
+                    {key: 'First and Foremost',                 sound: require('./native/sounds/first-and-foremost.mp3')},
+                    {key: 'Great Save',                         sound: require('./native/sounds/great-save.mp3')},
+                    {key: 'Oh No! Oh yeah!',                    sound: require('./native/sounds/oh-no-oh-yeah.mp3')},
+                    {key: 'Scoober Crossfield',                 sound: require('./native/sounds/scoober-crossfield.mp3')},
+                    {key: 'Stick Your Hand In',                 sound: require('./native/sounds/stick-your-hand-in.mp3')},
+                    {key: 'Too Slow',                           sound: require('./native/sounds/too-slow.mp3')},
+                    {key: 'What a look',                        sound: require('./native/sounds/what-a-look.mp3')},
                     {key: 'Dire, Dire Docks - Mario 64',        sound: require('./native/sounds/mario_64-dire_dire_docks.mp3')},
                     {key: 'Starting Race (First) - Mario Kart', sound: require('./native/sounds/mario_kart-starting_race_first.mp3')},
                     {key: 'Finish Line - Mario Kart',           sound: require('./native/sounds/mario_kart-finish_line.mp3')},
